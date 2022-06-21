99001437e24d04212af40a503ae31298cc2203fc
syaaav <mysyava@gmail.com>
2022-06-21 19:35:45 +0300

    Create my first Class Component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 // import App from "./App";
-import App from './Homework2'
+// import App from './Homework2'
+import MinMax from './Homework3'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <MinMax min={16} max={20} />
   </React.StrictMode>
 )
 

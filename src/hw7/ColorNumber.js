1d44e5c7502d91e480fb397e2c64c8d8ec30de91
syaaav <mysyava@gmail.com>
2022-07-14 14:32:26 +0300

    Improve checking functions

--- src/hw7/ColorNumber.js
+++ src/hw7/ColorNumber.js
@@ -2,22 +2,23 @@ import './NumberGenerator.css'
 import PropTypes from 'prop-types'
 
 export default function ColorNumbers(item) {
-  if (item.number !== 2 && item.number % 2 === 0) {
-    return <div className="green">{item.number}</div>
-  }
+  const simpleNums = [2, 3, 5, 7]
+  const isEven = (number) => number % 2 === 0
+  const isOdd = (number) =>
+    number === 1 || number % 3 === 0 || number % 5 === 0 || number % 7 === 0
+
   if (
-    item.number === 2 ||
-    item.number === 3 ||
-    item.number === 5 ||
-    item.number === 7 ||
-    (item.number !== 1 &&
-      item.number % 3 !== 0 &&
-      item.number % 5 !== 0 &&
-      item.number % 7 !== 0)
+    simpleNums.includes(item.number) ||
+    (!isOdd(item.number) && !isEven(item.number))
   ) {
     return <div className="red">{item.number}</div>
   }
-  return <div className="yellow">{item.number}</div>
+  if (isEven(item.number)) {
+    return <div className="green">{item.number}</div>
+  }
+  if (isOdd(item.number)) {
+    return <div className="yellow">{item.number}</div>
+  }
 }
 
 ColorNumbers.propTypes = {
